9cd6e7c2908a134efa69593fe583484eba0495db
Diovane Alves <alvdiovane@gmail.com>
2023-04-03 19:49:25 -0300

    :construction:	Criando Rotas para Home e Favoritos

---
+++ src/routes/index.js
@@ -0,0 +1,54 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import { Home } from "../pages/Home";
+import { Favorites } from "../pages/Favorites";
+
+const Tab = createBottomTabNavigator();
+
+export function Routes() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarActiveTintColor: false,
+        tabBarActiveTintColor: "#121212",
+
+        tabBarStyle: {
+          backgroundColor: "#FFFFFF",
+          borderTopWidth: 0,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="HomeTab"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => {
+            if (focused) {
+              return <Ionicons name="home" color="#000" size={size} />;
+            } else {
+              return <Ionicons name="home-outline" color={color} size={size} />;
+            }
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => {
+            if (focused) {
+              return <Ionicons name="heart" color="#FF4141" size={size} />;
+            } else {
+              return (
+                <Ionicons name="heart-outline" color={color} size={size} />
+              );
+            }
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
