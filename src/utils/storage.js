1a6dfa5fffe588b993f405e993308e8fd858091d
Diovane Alves <alvdiovane@gmail.com>
2023-04-05 21:31:04 -0300

    🚧 Armazenando as receita favoritas no Dispositivo

---
+++ src/utils/storage.js
@@ -0,0 +1,45 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export async function getFavorite(key) {
+  const favorites = await AsyncStorage.getItem(key);
+  return JSON.parse(favorites) || [];
+}
+
+export async function saveFavorite(key, newRecipe) {
+  let myFavorites = await getFavorite(key);
+  let hasItem = myFavorites.some((item) => item.id === newRecipe.id);
+
+  if (hasItem) {
+    alert("Esse item já esta salvo na sua lista");
+    return;
+  }
+
+  myFavorites.push(newRecipe);
+
+  await AsyncStorage.setItem(key, JSON.stringify(myFavorites));
+  alert("Salvo com sucesso");
+}
+
+export async function deleteFavorite(id) {
+  let recipes = await getFavorite("@appreceitas");
+
+  let myFavorites = recipes.filter((item) => {
+    return item.id !== id;
+  });
+
+  await AsyncStorage.setItem("@appreceitas", JSON.stringify(myFavorites));
+  alert("Item Deletado com Sucesso");
+  return myFavorites;
+}
+
+export async function isFavorite(recipe) {
+  let myRecipes = await getFavorite("@appreceitas");
+
+  const favorites = myRecipes.find((item) => item.id === recipe.id);
+
+  if (favorites) {
+    return true;
+  } else {
+    return false;
+  }
+}
